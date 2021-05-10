323e8e349975273eeaca428477842c9e7101200e
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2021-05-10 21:54:46 +0300

    hide desktop items scuf and fix

--- scuf.bat
+++ scuf.bat
@@ -1,2 +1,5 @@
-START D:\regedit\fix.reg
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoDesktop /t REG_DWORD /d  1
+
+START D:\regedit\scuf.reg
 START D:\web\index.html
