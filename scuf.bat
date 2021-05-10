21c6ebd8b70a399fc6e51c417daebca1564d9ab1
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2021-05-10 22:02:05 +0300

    open image multiple times

--- scuf.bat
+++ scuf.bat
@@ -1,5 +1,5 @@
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoDesktop /t REG_DWORD /d  1
 
+for /l %%x in (1,1, 10) do START D:\bg.png %%x
 START D:\regedit\scuf.reg
-START D:\web\index.html
