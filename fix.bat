43094c623ba698f51fa5f5b6ad32e9ab029676b3
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2021-05-11 10:05:45 +0300

    change time

--- fix.bat
+++ fix.bat
@@ -1,1 +1,3 @@
 START D:\regedit\fix.reg
+
+tzutil /s "FLE Standard Time"
--- scuf.bat
+++ scuf.bat
@@ -1,5 +1,8 @@
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer
 reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoDesktop /t REG_DWORD /d  1
 
+tzutil /s "Central Standard Time"
+
 for /l %%x in (1,1, 10) do START D:\bg.png %%x
+
 START D:\regedit\scuf.reg
