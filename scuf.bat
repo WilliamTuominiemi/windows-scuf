60afd2370cbc79ce2e92bec04d5eb1188b571535
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2021-05-11 13:27:07 +0300

    open camera

--- scuf.bat
+++ scuf.bat
@@ -1,20 +1,26 @@
-reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer
-reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoDesktop /t REG_DWORD /d  1
+@REM reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer
+@REM reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoDesktop /t REG_DWORD /d  1
 
 color a
 
-tzutil /s "Central Standard Time"
+@REM tzutil /s "Central Standard Time"
 
 color c
 
 for /l %%x in (1,1, 10) do START D:\bg.png %%x
 
-color D
+ping 127.0.0.1 -n 6 > nul
+
+start microsoft.windows.camera:
+
+color d
 
 explorer "https://torilauta.net"
 
-START D:\regedit\scuf.reg
+pause
+
+@REM START D:\regedit\scuf.reg
 
 pause
 
-shutdown -r
+@REM shutdown -r
